eab051057251bf4b6e2fa309de846c4707d27a0c
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-02 15:16:14 +0700

    Create navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,7 +5,25 @@ import PropTypes from 'prop-types'
 function Navbar({ title }) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-      Navbar
+      <div className='container mx-auto'>
+        <div className='flex-none px-2 mx-2'>
+          <FaGithub className='inline pr-2 text-3xl' />
+          <Link to='/' className='text-lg font-bold align-middle'>
+            {title}
+          </Link>
+        </div>
+
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
