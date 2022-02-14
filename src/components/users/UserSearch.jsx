51692f5b5c842a995f343ea5ea165aedc16a4741
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-14 12:10:32 +0700

    Refactor githubactions

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -7,7 +7,7 @@ function UserSearch() {
   const [text, setText] = useState('')
 
   // Pull out what we need from the context
-  const { users, dispatch, clearUsers } = useContext(GithubContext)
+  const { users, dispatch } = useContext(GithubContext)
   const { setAlert } = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value)
@@ -50,7 +50,10 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button className='btn btn-ghost btn-lg' onClick={clearUsers}>
+          <button
+            className='btn btn-ghost btn-lg'
+            onClick={() => dispatch({ type: 'CLEAR_USERS' })}
+          >
             Clear
           </button>
         </div>
