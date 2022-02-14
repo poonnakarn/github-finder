ff59c8bc3be8bcc520a9a2ac89741f2b04f8e509
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-14 22:01:30 +0700

    Using axios and merge action function

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,5 +1,4 @@
 import { createContext, useReducer } from 'react'
-import { createRoutesFromChildren } from 'react-router-dom'
 import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -17,16 +17,23 @@ const githubReducer = (state, action) => {
         ...state,
         loading: true,
       }
-    case 'GET_USER':
+    // case 'GET_USER':
+    //   return {
+    //     ...state,
+    //     user: action.payload,
+    //     loading: false,
+    //   }
+    // case 'GET_REPOS':
+    //   return {
+    //     ...state,
+    //     repos: action.payload,
+    //     loading: false,
+    //   }
+    case 'GET_USER_AND_REPOS':
       return {
         ...state,
-        user: action.payload,
-        loading: false,
-      }
-    case 'GET_REPOS':
-      return {
-        ...state,
-        repos: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         loading: false,
       }
     default:
